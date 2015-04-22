90fbf3c15c47421ff1c509cb58c05d7f118b8c0e
F481 <fabi.kcirf@gmail.com>
2015-04-22 20:34:43 +0200

    add logged in users to highscore: - append table rows when new users logs in - if no user name is set show a error alert - set height: auto for large number of players

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,49 @@
+var playerCounter = 0;
+
 $(document).ready(function() {
-    $('marquee').marquee();
+    $(".alert, #overlay, #loader").hide();
+
+    $('.login-btn').on('click', function() {
+        addPlayerToList($('.login-input').val())
+    });
+
 });
+
+function addPlayerToList(playerName) {
+    if (playerName == "") {
+        openAlert('danger', 'Bitte geben Sie einen Spielernamen an!'); return;
+    }
+
+    if (playerCounter == 0) {
+        $('#highscore table tbody tr td:first').text(1);
+        $('#highscore table tbody tr td:nth-child(2)').text(playerName);
+        $('#highscore table tbody tr td:nth-child(3)').text(0);
+    } else {
+        $('#highscore table tbody:last').append('<tr><td>' + parseInt(playerCounter + 1) + '</td><td>' + playerName + '</td><td>0</td></tr>')
+    }
+
+    playerCounter = playerCounter + 1;
+}
+
+function showOverlay(show) {
+    show ? $("#overlay").show() : $("#overlay").hide();
+}
+
+function openAlert(type, text) {
+    showOverlay(true);
+
+    switch (type) {
+        case "success":
+            $(".alert-success p").text(text);
+            $(".alert-success").show();
+            break;
+        case "danger":
+            $(".alert-danger p").text(text);
+            $(".alert-danger").show();
+    }
+
+    $(".alert .close:button").click(function () {
+        $(".alert").hide();
+        showOverlay(false);
+    });
+};
